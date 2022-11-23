9f283b626128622d6e5a26ddb0158df7bb6be76e
ZenoVux <zenovux0702@gmail.com>
2022-11-23 12:37:37 +0700

    Update demo-java6-b3

---
+++ demo-java6-b3/src/main/java/sof306/ph18485/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package sof306.ph18485.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/home/index")
+	public String index(Model model) {
+		return "home/index";
+	}
+
+	@GetMapping("/home/about")
+	public String about(Model model) {
+		return "home/about";
+	}
+}
--- demo-java6-b3/src/main/java/sof306/ph18485/controllers/StudentController.java
+++ demo-java6-b3/src/main/java/sof306/ph18485/controllers/StudentController.java
@@ -2,6 +2,8 @@ package sof306.ph18485.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -18,7 +20,11 @@ public class StudentController {
 	}
 	
 	@GetMapping("/student/save")
-	public String save(@ModelAttribute("sv") Student sv) {
-		return "student/succes";
+	public String save(Model model, @Validated @ModelAttribute("sv") Student sv, Errors errors) {
+		if(errors.hasErrors()) {
+			model.addAttribute("messgae","Vui lòng sửa các lỗi sau");
+			return "student/form";
+		}
+		return "student/success";
 	}
 }
