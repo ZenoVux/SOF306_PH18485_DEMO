1e70675eb1f946568785eea29960e0ab3ef4c426
ZenoVux <zenovux0702@gmail.com>
2022-11-23 09:57:43 +0700

    Update demo-java6-b7

--- demo-java6-b7/src/main/java/sof306/ph18485/SecurityConfig.java
+++ demo-java6-b7/src/main/java/sof306/ph18485/SecurityConfig.java
@@ -1,5 +1,6 @@
 package sof306.ph18485;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,10 +11,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
+import sof306.ph18485.services.UserService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserService userService;
+	
 	@Bean
 	public BCryptPasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -21,15 +27,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService);
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		Tắt csrf và cros 6.2
+//		Tắt csrf và cros
 		http.csrf().disable().cors().disable();
-		
+
+//		Phân quyền
 		http.authorizeRequests()
+			.antMatchers("/home/admins").hasRole("ADMIN")
+			.antMatchers("/home/users").hasAnyRole("ADMIN", "USER")
+			.antMatchers("/home/authenticated").authenticated()
 			.anyRequest().permitAll();
 		
+//		Kiểm tra lỗi truy cập
+		http.exceptionHandling()
+			.accessDeniedPage("/auth/access/denied");
+
+//		Thay đổi trang đăng nhập
+		http.formLogin()
+			.loginPage("/auth/login/form")
+			.loginProcessingUrl("/auth/login")
+			.defaultSuccessUrl("/auth/login/success", false)
+			.failureUrl("/auth/login/error")
+			.usernameParameter("username")
+			.passwordParameter("password");
+		
+		http.rememberMe()
+			.rememberMeParameter("remember");
+		
+//		Đăng xuất
+		http.logout()
+			.logoutUrl("/auth/logout")
+			.logoutSuccessUrl("/auth/logout/success")
+			.addLogoutHandler(new SecurityContextLogoutHandler());
+		
+//		Đăng nhập bằng MXH
+		http.oauth2Login()
+			.loginPage("/auth/login/form")
+			.defaultSuccessUrl("/oauth2/login/success")
+			.failureUrl("/auth/login/error")
+			.authorizationEndpoint()
+			.baseUri("/oauth2/authorization");
 	}
 }
--- demo-java6-b7/src/main/java/sof306/ph18485/entities/Account.java
+++ demo-java6-b7/src/main/java/sof306/ph18485/entities/Account.java
@@ -1,9 +1,14 @@
 package sof306.ph18485.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -23,4 +28,7 @@ public class Account {
 	private String email;
 	@Column(name = "photo")
 	private String photo;
+	@JsonIgnore
+	@OneToMany(mappedBy = "account")
+	private List<Authority> authorities;
 }
